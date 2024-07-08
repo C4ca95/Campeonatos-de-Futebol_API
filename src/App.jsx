2ee392a229ade9d7e10c56bbd628723aed968d16
Rafael Castro <rafaelpereiradecastro06@gmail.com>
2024-07-08 16:16:00 -0300

    Login Front React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login.jsx';
+import './App.css'; // Certifique-se de ter um arquivo de estilo apropriado
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
